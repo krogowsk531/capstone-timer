fbcbb12b3b6226485418aeb30db296aacc3d447e
katystsauveur <katyrogowski@yahoo.com>
2022-04-08 09:11:51 -0700

    Add user model

---
+++ src/main/java/com/optum/timer/model/User.java
@@ -0,0 +1,42 @@
+package com.optum.timer.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private char[] password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+}
