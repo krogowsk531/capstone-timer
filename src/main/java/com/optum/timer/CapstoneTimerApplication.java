edaa963a907c872ba904b1708eb0213ffdd8797d
katystsauveur <katyrogowski@yahoo.com>
2022-04-08 08:10:18 -0700

    Initial commit

---
+++ src/main/java/com/optum/timer/CapstoneTimerApplication.java
@@ -0,0 +1,13 @@
+package com.optum.timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneTimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optum/timer/CapstoneTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optum.timer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneTimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
