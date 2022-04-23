d61e3915303260b634ebb9506db6fe50c3497c38
katystsauveur <katyrogowski@yahoo.com>
2022-04-23 07:40:27 -0700

    Built comment model

---
+++ src/main/java/com/optum/timer/model/Comment.java
@@ -0,0 +1,61 @@
+package com.optum.timer.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.Objects;
+
+@Entity
+@Table(name="comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="future_intention")
+    private String futureIntention;
+
+    @Column(name  = "description")
+    private String description;
+
+    public Comment() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFutureIntention() {
+        return futureIntention;
+    }
+
+    public void setFutureIntention(String futureIntention) {
+        this.futureIntention = futureIntention;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return id == comment.id && Objects.equals(futureIntention, comment.futureIntention) && Objects.equals(description, comment.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, futureIntention, description);
+    }
+}
